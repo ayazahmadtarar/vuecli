ec18bbe3b0c94b991d58f201fd2910974ff68cc4
ayazahmadtarar <ayazahmad@ymail.com>
2020-05-01 15:45:23 +0500

    Deployment test

---
+++ src/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12">
+            <header>
+                <h1>Server Status {{title}}</h1>
+            </header>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+	props: {
+	  title: { 
+        type: Object,
+        required: true
+    }
+	}
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/ServerStatus.vue
@@ -0,0 +1,21 @@
+
+<template>
+	<div>
+		<p>This status is {{status}}</p>
+		<button @click="changeevent">Change Status</button>
+	</div>
+</template>
+<script>
+	export default {
+      data: function() {
+      	return{
+      	status: "Critical"
+      	}
+       },
+       methods:{
+       	changeevent(){
+       		this.status = "Normal"
+       	}
+       }
+	};
+</script>
